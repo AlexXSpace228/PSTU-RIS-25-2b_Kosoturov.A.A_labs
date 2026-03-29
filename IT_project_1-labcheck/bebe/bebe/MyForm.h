e80b93d9edf8b2b9471c68b1f2e8afa6794bc592
Косотуров Александр РИС-25-2б <aleksandrstreleckij7@gmail.com>
2026-03-29 23:02:40 +0500

    нормальное добавление

--- IT_project_1-labcheck/bebe/bebe/MyForm.h
+++ IT_project_1-labcheck/bebe/bebe/MyForm.h
@@ -24,7 +24,13 @@ namespace bebe {
 			//TODO: добавьте код конструктора
 			//
 		}
-
+	public:
+		String^ labName;
+		String^ deadline;
+	private: System::Windows::Forms::Button^ button1;
+	public:
+		String^ comment;
+		String^ tasks;
 	protected:
 		/// <summary>
 		/// Освободить все используемые ресурсы.
@@ -43,6 +49,12 @@ namespace bebe {
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::CheckBox^ checkBox2;
+	private: System::Windows::Forms::CheckBox^ checkBox3;
+	private: System::Windows::Forms::CheckBox^ checkBox4;
+
 
 	private:
 		/// <summary>
@@ -63,68 +75,156 @@ namespace bebe {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox3 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox4 = (gcnew System::Windows::Forms::CheckBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(58, 44);
+			this->textBox1->Location = System::Drawing::Point(108, 100);
+			this->textBox1->Margin = System::Windows::Forms::Padding(4);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(179, 20);
+			this->textBox1->Size = System::Drawing::Size(296, 22);
 			this->textBox1->TabIndex = 0;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm1::textBox1_TextChanged);
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(58, 102);
+			this->textBox2->Location = System::Drawing::Point(108, 145);
+			this->textBox2->Margin = System::Windows::Forms::Padding(4);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(179, 20);
+			this->textBox2->Size = System::Drawing::Size(296, 22);
 			this->textBox2->TabIndex = 1;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm1::textBox2_TextChanged);
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(58, 154);
+			this->textBox3->Location = System::Drawing::Point(108, 186);
+			this->textBox3->Margin = System::Windows::Forms::Padding(4);
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(179, 20);
+			this->textBox3->Size = System::Drawing::Size(296, 22);
 			this->textBox3->TabIndex = 2;
+			this->textBox3->TextChanged += gcnew System::EventHandler(this, &MyForm1::textBox3_TextChanged);
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(122, 28);
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(179, 50);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(52, 13);
+			this->label1->Size = System::Drawing::Size(86, 22);
 			this->label1->TabIndex = 3;
-			this->label1->Text = L"Введите ";
+			this->label1->Text = L"Введите:";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(122, 86);
+			this->label2->Location = System::Drawing::Point(27, 148);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(52, 13);
+			this->label2->Size = System::Drawing::Size(64, 16);
 			this->label2->TabIndex = 4;
-			this->label2->Text = L"Введите ";
+			this->label2->Text = L"Дедлайн";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(122, 138);
+			this->label3->Location = System::Drawing::Point(4, 189);
+			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(52, 13);
+			this->label3->Size = System::Drawing::Size(96, 16);
 			this->label3->TabIndex = 5;
-			this->label3->Text = L"Введите ";
+			this->label3->Text = L"Комментарий";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(27, 106);
+			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(73, 16);
+			this->label4->TabIndex = 6;
+			this->label4->Text = L"Название";
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(41, 300);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(69, 20);
+			this->checkBox1->TabIndex = 7;
+			this->checkBox1->Text = L"Отчет";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			this->checkBox1->CheckedChanged += gcnew System::EventHandler(this, &MyForm1::checkBox1_CheckedChanged);
+			// 
+			// checkBox2
+			// 
+			this->checkBox2->AutoSize = true;
+			this->checkBox2->Location = System::Drawing::Point(41, 274);
+			this->checkBox2->Name = L"checkBox2";
+			this->checkBox2->Size = System::Drawing::Size(102, 20);
+			this->checkBox2->TabIndex = 8;
+			this->checkBox2->Text = L"Блок схема";
+			this->checkBox2->UseVisualStyleBackColor = true;
+			this->checkBox2->CheckedChanged += gcnew System::EventHandler(this, &MyForm1::checkBox2_CheckedChanged);
+			// 
+			// checkBox3
+			// 
+			this->checkBox3->AutoSize = true;
+			this->checkBox3->Location = System::Drawing::Point(161, 274);
+			this->checkBox3->Name = L"checkBox3";
+			this->checkBox3->Size = System::Drawing::Size(53, 20);
+			this->checkBox3->TabIndex = 9;
+			this->checkBox3->Text = L"Код";
+			this->checkBox3->UseVisualStyleBackColor = true;
+			this->checkBox3->CheckedChanged += gcnew System::EventHandler(this, &MyForm1::checkBox3_CheckedChanged);
+			// 
+			// checkBox4
+			// 
+			this->checkBox4->AutoSize = true;
+			this->checkBox4->Location = System::Drawing::Point(161, 300);
+			this->checkBox4->Name = L"checkBox4";
+			this->checkBox4->Size = System::Drawing::Size(66, 20);
+			this->checkBox4->TabIndex = 10;
+			this->checkBox4->Text = L"IDEF0";
+			this->checkBox4->UseVisualStyleBackColor = true;
+			this->checkBox4->CheckedChanged += gcnew System::EventHandler(this, &MyForm1::checkBox4_CheckedChanged);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(183, 340);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(62, 23);
+			this->button1->TabIndex = 11;
+			this->button1->Text = L"OK";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm1::button1_Click);
 			// 
 			// MyForm1
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(444, 375);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->checkBox4);
+			this->Controls->Add(this->checkBox3);
+			this->Controls->Add(this->checkBox2);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"MyForm1";
 			this->Text = L"MyForm1";
 			this->Load += gcnew System::EventHandler(this, &MyForm1::MyForm1_Load);
@@ -135,7 +235,43 @@ namespace bebe {
 #pragma endregion
 	private: System::Void MyForm1_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+		String^ name = textBox1->Text;
+	}
+	private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+		String^ ded = textBox2->Text;
+	}
+	private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+		String^ comm = textBox3->Text;
+	}
+private: System::Void checkBox1_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	//отчет
+}
+private: System::Void checkBox2_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	//блок схема
+}
+private: System::Void checkBox3_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	//код
+}
+private: System::Void checkBox4_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	//idef0
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	//OK
+	labName = textBox1->Text;
+	deadline = textBox2->Text;
+	comment = textBox3->Text;
+	tasks = "";
+
+	if (checkBox1->Checked) tasks += "Отчет ";
+	if (checkBox2->Checked) tasks += "Блок-схема ";
+	if (checkBox3->Checked) tasks += "Код ";
+	if (checkBox4->Checked) tasks += "IDEF0 ";
+
+	this->DialogResult = System::Windows::Forms::DialogResult::OK;
+	this->Close();
+}
+};
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
@@ -216,9 +352,6 @@ namespace bebe {
 			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
 			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
-			// 
-			// listView1
-			// 
 			this->listView1->HideSelection = false;
 			this->listView1->Location = System::Drawing::Point(0, 27);
 			this->listView1->Name = L"listView1";
@@ -226,30 +359,18 @@ namespace bebe {
 			this->listView1->TabIndex = 6;
 			this->listView1->UseCompatibleStateImageBehavior = false;
 			this->listView1->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm::listView1_SelectedIndexChanged);
-			// 
-			// ghToolStripMenuItem
-			// 
 			this->ghToolStripMenuItem->Name = L"ghToolStripMenuItem";
 			this->ghToolStripMenuItem->Size = System::Drawing::Size(65, 20);
 			this->ghToolStripMenuItem->Text = L"Справка";
 			this->ghToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::ghToolStripMenuItem_Click);
-			// 
-			// helpToolStripMenuItem
-			// 
 			this->helpToolStripMenuItem->Name = L"helpToolStripMenuItem";
 			this->helpToolStripMenuItem->Size = System::Drawing::Size(44, 20);
 			this->helpToolStripMenuItem->Text = L"Help";
 			this->helpToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::helpToolStripMenuItem_Click);
-			// 
-			// добавитьРаботуToolStripMenuItem
-			// 
 			this->добавитьРаботуToolStripMenuItem->Name = L"добавитьРаботуToolStripMenuItem";
 			this->добавитьРаботуToolStripMenuItem->Size = System::Drawing::Size(112, 20);
 			this->добавитьРаботуToolStripMenuItem->Text = L"Добавить работу";
 			this->добавитьРаботуToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::добавитьРаботуToolStripMenuItem_Click);
-			// 
-			// menuStrip1
-			// 
 			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
 				this->ghToolStripMenuItem,
 					this->helpToolStripMenuItem, this->добавитьРаботуToolStripMenuItem
@@ -260,9 +381,6 @@ namespace bebe {
 			this->menuStrip1->TabIndex = 9;
 			this->menuStrip1->Text = L"menuStrip1";
 			this->menuStrip1->ItemClicked += gcnew System::Windows::Forms::ToolStripItemClickedEventHandler(this, &MyForm::menuStrip1_ItemClicked);
-			// 
-			// MyForm
-			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
@@ -296,6 +414,7 @@ private: System::Void MyForm_Resize(System::Object^ sender, System::EventArgs^ e
 	);
 }
 private: System::Void listView1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+
 }
 private: System::Void ghToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
 	MessageBox::Show(
@@ -313,19 +432,27 @@ private: System::Void helpToolStripMenuItem_Click(System::Object^ sender, System
 	MessageBox::Show("Работа с программой:");
 }
 private: System::Void добавитьРаботуToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
-	int id = listView1->Items->Count + 1;
 
-	ListViewItem^ item = gcnew ListViewItem(id.ToString());
-
-	item->SubItems->Add("Новая лабораторная");
-	item->SubItems->Add("not started");
-	item->SubItems->Add("—");
-	item->SubItems->Add("");
-
-	listView1->Items->Add(item);
-	
 	MyForm1^ form = gcnew MyForm1();
-	form->ShowDialog();
+
+	if (form->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+	{
+		int id = listView1->Items->Count + 1;
+
+		ListViewItem^ item = gcnew ListViewItem(id.ToString());
+
+		item->SubItems->Add(form->labName);
+		item->SubItems->Add("done");
+		item->SubItems->Add(form->deadline);
+		item->SubItems->Add(form->comment);
+		item->SubItems->Add(form->tasks);
+
+		// Цвет лабы в списке
+		item->BackColor = System::Drawing::Color::White;
+
+		listView1->Items->Add(item);
+	}
+
 }
 private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
 }
