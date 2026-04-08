8be42da4fe43a2325727da5037fa6963018fbca9
AlexXSpace228 <aleksandrstreleckij7@gmail.com>
2026-04-08 18:21:29 +0500

    sdfhdfjfghjkhg

---
+++ IT_project_1-labcheck/bebe/bebe/LABS.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+class LABS
+{
+private:
+	std::string PATH;
+	std::string name;
+	//std::string deadline;
+	std::string comm;
+	bool BS;
+	bool report;
+	bool code;
+	bool IDEF0;
+public:
+	LABS(std::string PATHconstr, std::string nameconstr, /*std::string deadlineconstr;*/ std::string commconstr, bool BSconstr, bool reportconstr, bool codeconstr, bool IDEF0constr)
+	{
+		PATH = PATHconstr, name = nameconstr, /*deadline = deadlineconstr,*/ comm = commconstr, BS = BSconstr, report = reportconstr, code = codeconstr, IDEF0 = IDEF0constr;
+	}
+	LABS() {
+		PATH = "PATHconstr", name = "nameconstr", /*deadline = deadlineconstr,*/ comm = "commconstr", BS = false, report = false, code = false, IDEF0 = false;
+	}
+	const std::string GET_PATH() {
+		return PATH;
+	}
+	const std::string GET_name() {
+		return name;
+	}
+	///const std::string GET_PATH() {
+	///return PATH;
+	///}
+	const std::string GET_comm() {
+		return comm;
+	}
+	const bool GET_BS() {
+		return BS;
+	}
+	const bool GET_report() {
+		return report;
+	}
+	const bool GET_code() {
+		return code;
+	}
+	const bool GET_IDEF0() {
+		return IDEF0;
+	}
+	~LABS() {};
+};
+
--- Сourse1_Semester2_Labs/sorting_methods/Sort_methods/Sort_methods/Source.cpp
+++ Сourse1_Semester2_Labs/sorting_methods/Sort_methods/Sort_methods/Source.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <string>
+#include <fstream>
 #include <cstdlib>
 <<<<<<< HEAD
 
@@ -155,6 +157,118 @@ static int partitionLomuto(std::vector <int>& arr, int low, int high) {
     return pI;
 }
 
+void Distribution(std::string path1, std::string path2, std::string main_path) {
+    std::ofstream f1, f2;
+    std::ifstream mainf;
+    std::string temp, cur;
+    int itemp, icur;
+    f1.open(path1);
+    f2.open(path2);
+    mainf.open(main_path);
+    if (!f1.is_open() || !f2.is_open()) { std::cout << "Error" << std::endl; }
+    else {
+        bool firstfile = true;
+        mainf >> cur;
+        while (!mainf.eof()) {
+            if (firstfile) {
+                f1 << cur << '\n';
+                temp = cur;
+                mainf >> cur;
+                if (mainf.eof()) {
+                    f1 << '/' << '\n';
+                    return;
+                }
+                itemp = stoi(temp);
+                icur = stoi(cur);
+                while (itemp <= icur) {
+                    if (mainf.eof()) {
+                        f1 << '/' << '\n';
+                        return;
+                    }
+                    f1 << cur << '\n';
+                    temp = cur;
+                    mainf >> cur;
+                    itemp = stoi(temp);
+                    icur = stoi(cur);
+                }
+                f1 << '/' << '\n';
+                firstfile = false;
+            }
+            else {
+                f2 << cur << '\n';
+                temp = cur;
+                mainf >> cur;
+                if (mainf.eof()) {
+                    f2 << '/' << '\n';
+                    return;
+                }
+                itemp = stoi(temp);
+                icur = stoi(cur);
+                while (itemp <= icur) {
+                    if (mainf.eof()) {
+                        f2 << '/' << '\n';
+                    }
+                    f2 << cur << '\n';
+                    temp = cur;
+                    mainf >> cur;
+                    itemp = stoi(temp);
+                    icur = stoi(cur);
+                }
+                f2 << '/' << '\n';
+                firstfile = true;
+            }
+        }
+    }
+    f1.close();
+    f2.close();
+    mainf.close();
+}
+
+void Merge(std::string path1, std::string path2, std::string main_path) {
+    Distribution(path1, path2, main_path);
+    std::ifstream f1, f2;
+    std::ofstream mainf;
+    std::string temp1, temp2;
+    int itemp1, itemp2;
+    f1.open(path1);
+    f2.open(path2);
+    mainf.open(main_path);
+    if (!f1.is_open() || !f2.is_open()) { std::cout << "Error" << std::endl; }
+    else {
+        int i = 0, j = 0;
+        while (!(f1.eof())) {
+            f1 >> temp1;
+            f2 >> temp2;
+            if (temp2 == "") return;
+            while (temp1 != "/" || temp2 != "/") {
+                if (temp1 == "/") {
+                    mainf << temp2 << "\n";
+                    f2 >> temp2;
+                }
+                else if (temp2 == "/") {
+                    mainf << temp1 << "\n";
+                    f1 >> temp1;
+                }
+                else {
+                    itemp1 = stoi(temp1);
+                    itemp2 = stoi(temp2);
+                    if (itemp1 <= itemp2) {
+                        mainf << temp1 << "\n";
+                        f1 >> temp1;
+                    }
+                    else {
+                        mainf << temp2 << "\n";
+                        f2 >> temp2;
+                    }
+                }
+            }
+        }
+    }
+    f1.close();
+    f2.close();
+    mainf.close();
+    Merge(path1, path2, main_path);
+}
 
 static void MASSINPUTRAND(std::vector <int>& arr) {
     int n;
@@ -168,6 +282,7 @@ static void MASSINPUTRAND(std::vector <int>& arr) {
 int main() {
     srand(time(NULL));
     setlocale(LC_ALL, "RU");
+    system("color E0");
 
     int len;
     std::vector <int> mass;
