0acedf0a066799bddd35fe6da237fffeeb20d755
AlexXSpace228 <aleksandrstreleckij7@gmail.com>
2026-04-12 16:35:17 +0500

    1 лабораторная по ООП - 11 вариант

---
+++ Сourse1_Semester2_Labs/laboratory_works/LAB1-newlabs-11var/LAB1-OOP-11var/LAB1-OOP-11var/Class_A.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+#include <iostream>
+#include <cstdlib>
+
+class A {
+private:
+	double first; // оклад
+	int second; // количество отработанных дней
+public:
+	// конструктор
+	A() = default;
+
+	void Read() {
+		double f;
+		int s;
+		do {
+			std::cout << "Введите оклад и дни: ";
+			std::cin >> f >> s;
+		} while (!Init(f, s)); // Повторяем, пока не введут корректно
+	}
+
+	bool Init(double f, int s) {
+		if (f < 0.0 || s < 0) {
+			return false;
+		}
+		first = f;
+		second = s;
+		return true;
+	}
+
+	void Show() const{
+		std::cout << "============= КЛАСС А =============\n";
+		std::cout << "оклад: " << first << '\n';
+		std::cout << "количество отработанных дней : " << second << '\n';
+	}
+
+	int summa(int day_in_month) const {
+		if (day_in_month == 0) return 0;
+		return first / day_in_month  * second;
+	}
+};
+
+A make_A(double f, int s) {
+	A obj;
+	if (!obj.Init(f, s)) {
+		std::cout << "Ошибка: неверные параметры для создания объекта!" << std::endl;
+		exit(1);
+	}
+	return obj;
+}
---
+++ Сourse1_Semester2_Labs/laboratory_works/LAB1-newlabs-11var/LAB1-OOP-11var/LAB1-OOP-11var/Source.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <clocale>
+#include <vector>
+#include "Class_A.h"
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	A something1;
+	A* something2 = new A;
+	std::vector <A> Array_Of_A{};
+
+	something1 = make_A(50000.0, 15);
+	something2->Read();
+
+	double day_in_month_now = 30;
+
+	something1.Show();
+	std::cout << "Оклад 1: " << something1.summa(day_in_month_now) << '\n';
+	something2->Show();
+	std::cout << "Оклад 2: " << something2->summa(day_in_month_now) << '\n';
+
+	int LEN;
+	std::cout << "=====Массив данных класса А=====\n";
+	std::cout << "Сколько элементов нужно ввести: ";
+	std::cin >> LEN;
+
+	for (int i = 0; i < LEN; i++) {
+		A something;
+		something.Read();
+		Array_Of_A.push_back(something);
+	}
+
+	for (int i = 0; i < LEN; i++) {
+		Array_Of_A[i].Show();
+		std::cout << "Оклад " << (i+1) << ": " << Array_Of_A[i].summa(day_in_month_now) << '\n';
+	}
+
+	return 0;
+}
