093dca9ca9c5f0e9a6d7d458f20fd6b2f2c43a08
AlexXSpace228 <aleksandrstreleckij7@gmail.com>
2026-04-16 19:08:53 +0500

    laba4

---
+++ Сourse1_Semester2_Labs/laboratory_works/LAB1-newlabs-11var/LAB4-11var/LAB4-11var/ClassTrial.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+class TRIAL {
+private:
+
+	int first;
+	int second;
+	int third;
+
+	//конструкторы
+	TRIAL(int a1, int a2, int a3) {
+		first = a1;
+		second = a2;
+		third = a3;
+	}
+	TRIAL() {
+		first = 0;
+		second = 0;
+		third = 0;
+	}
+	TRIAL(const TRIAL& other) {
+		first = other.first;
+		second = other.second;
+		third = other.third;
+	}
+
+
+
+	~TRIAL();
+};
