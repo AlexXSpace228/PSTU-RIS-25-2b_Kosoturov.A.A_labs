f04dcc911a985a6176bafd6c60f8abe4e8f3876f
Косотуров Александр РИС-25-2б <aleksandrstreleckij7@gmail.com>
2026-04-16 18:53:43 +0500

    лабы

---
+++ Сourse1_Semester2_Labs/laboratory_works/LAB1-newlabs-11var/LAB2-11var/LAB2-11var/Class_Automobile.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <string>
+
+class AUTOMOBILE {
+private:
+	std::string mark;
+	std::string model;
+	int cost;
+public:
+	AUTOMOBILE(std::string ma, std::string mo, int c) {
+		mark = ma;
+		model = mo;
+		cost = c;
+	}
+	AUTOMOBILE() {
+		mark = "Default";
+		model = "Default";
+		cost = 0;
+	}
+	AUTOMOBILE(const AUTOMOBILE& other) {
+		mark = other.mark;
+		model = other.model;
+		cost = other.cost;
+	}
+	//сетеры
+	void setmark(std::string MARK) {
+		mark = MARK;
+	}
+	void setmark(std::string MODEL) {
+		model = MODEL;
+	}
+	void setmark(int COST) {
+		cost = COST;
+	}
+	// гетеры
+	std::string getmark() const{
+		return mark;
+	}
+	std::string getmark() const{
+		return model;
+	}
+	int getcost() const {
+		return cost;
+	}
+
+	~AUTOMOBILE();
+};
---
+++ Сourse1_Semester2_Labs/laboratory_works/LAB1-newlabs-11var/LAB3-11var/LAB3-11var/class_Money.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+
+class Money {
+private:
+	long roubles;
+	int peny;
+public:
+	Money() {
+		roubles = 0;
+		peny = 0;
+	}
+	Money(long r, int p) {
+		roubles = r;
+		peny = p;
+	}
+	Money(const Money& other) {
+		roubles = other.roubles;
+		peny = other.peny;
+	}
+
+	// сетеры
+	
+	void setRoubles(long R) {
+		roubles = R;
+	}
+	void setPeny(long P) {
+		peny = P;
+	}
+
+	// гетеры
+
+	long GetRUB() {
+		return roubles;
+	}
+	int GetP() {
+		return peny;
+	}
+
+	Money operator ++(int) {
+		peny++;
+	}
+	Money operator++ (int) {
+		peny++;
+	}
+
+	Money operator <(int) {
+		peny++;
+	}
+	Money operator >(int) {
+		peny++;
+	}
+
+	~Money();
+};
